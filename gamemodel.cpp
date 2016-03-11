258f75017a168069720a50282183f0ab24ea30ad
krath912 <krath912@students.bju.edu>
2016-03-11 18:42:59 -0500

    Added files via upload

---
+++ gamemodel.cpp
@@ -0,0 +1,19 @@
+#include "gamemodel.h"
+
+#include <QTextStream>
+
+gameModel::gameModel()
+{
+
+
+}
+
+//saves a game object
+void loadnsave::saveGame() {
+
+}
+
+//loads a game object
+void loadnsave::loadGame() {
+
+}
---
+++ gamemodel.h
@@ -0,0 +1,19 @@
+#ifndef GAMEMODEL_H
+#define GAMEMODEL_H
+
+
+
+class loadnsave {
+    virtual void saveGame();
+    virtual void loadGame();
+};
+
+class gameModel
+{
+public:
+    gameModel();
+};
+
+
+
+#endif // GAMEMODEL_H
